4a7db7ac9829d54c2d8f09cf07f5e2838bf3e36f
Timo Mäki <timo.j.maki@helsinki.fi>
2015-03-15 23:18:10 +0200

    iFailerAgain

--- OhtuVarasto/src/test/java/ohtu/ohtuvarasto/VarastoTest.java
+++ OhtuVarasto/src/test/java/ohtu/ohtuvarasto/VarastoTest.java
@@ -115,4 +115,9 @@ public class VarastoTest {
         final double ahneOtto = varasto.otaVarastosta(tryTake);
         assertEquals(result, ahneOtto, vertailuTarkkuus);
     }
+
+    @Test
+    public void failer() {
+        fail(":(");
+    }
 }
